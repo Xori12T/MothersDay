747df2639909b72c74292d686db6454a1d98c9a6
Xori12T <mistermaster1013@gmail.com>
2026-05-10 22:55:56 -0700

    Now final, changed heic to jpg, fixed text

--- scripts.js
+++ scripts.js
@@ -1,5 +1,19 @@
-window.githubPushes = 13;
+window.githubPushes = 15;
 
 document.querySelectorAll(".github-push-count").forEach((pushCounter) => {
     pushCounter.textContent = window.githubPushes;
+});
+
+document.querySelectorAll(".external-message").forEach(async (el) => {
+    const file = el.dataset.textSource;
+
+    try {
+        const response = await fetch(file);
+        const text = await response.text();
+
+        el.textContent = text;
+    } catch (err) {
+        el.textContent = "Could not load text.";
+        console.error(err);
+    }
 });
